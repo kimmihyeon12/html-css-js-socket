d9399867e90ce31a1da44ce9f1630004d0709909
mihyeon <2_5_3_1@naver.com>
2021-11-16 17:23:14 +0900

    ui update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,17 @@ module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.{js,html,ejs}', './src/**/*.{js,jsx,ts,tsx,vue}'],
   plugins: [],
+  theme:{
+    fontFamily: {
+      neob: ['NeoB'],
+      neoeb: ['NeoEB'],
+      neoh: ['NeoH'],
+      neol: ['NeoL'],
+      neom: ['NeoM'],
+      neor: ['NeoR'],
+      neosb: ['NeoSB'],
+      neot: ['NeoT'],
+      neoul: ['NeoUL'],
+    },
+  }
 }
