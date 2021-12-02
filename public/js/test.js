a4550dfdfc5b46155084a4bd7849e8b732df5ddb
mihyeon <2_5_3_1@naver.com>
2021-12-02 17:21:22 +0900

    caleder up

--- public/js/test.js
+++ public/js/test.js
@@ -1,85 +1,153 @@
-const container = document.querySelector('.test-contanier')
-const maxCount = 7
-//몇번째 주 인지
-const weekly = 1
-//주에서 앞에 몇일 있는지
-const preCount = 2
-let getCount = 10
+import SVG from "https://cdn.skypack.dev/svg.js";
+import {
+  spline,
+  map
+} from "https://cdn.skypack.dev/@georgedoescode/generative-utils@1.0.0";
+import { Vector2D } from "https://cdn.skypack.dev/@georgedoescode/vector2d@1.0.1";
+import debounce from "https://cdn.skypack.dev/debounce@1.2.1";
 
-let divTotalCount = preCount + getCount
+let mousePos = new Vector2D(0, 0);
+window.mouseHasMoved = false;
 
-let divCount = []
-// divCount.push(getCount)
-// const divEl = document.querySelectorAll('.test-contanier div div')
-const divColorEl = document.querySelectorAll('.color-box div')
-// let weeklyDiv = []
-// divEl.forEach((e, index) => {
-//   if (7 * (weekly - 1) < index + 1 && 7 * weekly > index) weeklyDiv.push(e)
-// })
-// weeklyDiv.forEach((w) => {
-//   console.log(w)
+const svg = SVG(document.querySelector("svg"));
+const path = document.getElementById("baseBtnPath");
 
-// })
-// divColorEl.forEach((e) => {
-//   console.log(e)
-// })
-getDivCount()
+const pointOrigins = pointsInPath(path, 32);
+const points = pointsInPath(path, 32);
+const bgPoints = pointsInPath(path, 32);
 
-function getDivCount() {
-  if (getCount > 7) {
-    let count =
-      getCount -
-      7 * (parseInt(divTotalCount / 7) - 1) -
-      (divTotalCount - 7 * parseInt(divTotalCount / 7))
-    divCount.push(count)
-    getCount = getCount - count
-  } else {
-    if (divTotalCount >= 7) {
-      divCount.push(7 - preCount)
-      getCount = getCount + 1 - preCount
-    } else {
-      divCount.push(getCount)
-    }
+let btnPathData = spline(points, 1, true);
+let bgPathData = spline(bgPoints, 1, true);
+
+const bgPath = svg
+  .path(btnPathData)
+  .fill("#A78BFA")
+  .attr("id", "bgPath")
+  .attr("filter", "url(#shadow)");
+const btnPath = svg
+  .path(btnPathData)
+  .fill("url(#gradient)")
+  .attr("id", "btnPath");
+
+const circles = [];
+
+points.forEach((p) => {
+  circles.push(
+    svg.circle(4).cx(p.x).cy(p.y).fill("#4C1D95").attr("class", "debug-dot")
+  );
+});
+
+function pointsInPath(path, numPoints = 10) {
+  const pathLength = path.getTotalLength();
+  const step = pathLength / numPoints;
+  const points = [];
+
+  for (let i = 0; i < pathLength; i += step) {
+    points.push(path.getPointAtLength(i));
   }
-  for (let i = 0; i < parseInt(divTotalCount / 7); i++) {
-    if (getCount >= 7) {
-      divCount.push(7)
-      getCount = getCount - 7
-    } else {
-      if (getCount != 0) divCount.push(getCount)
-    }
-  }
-  console.log(divCount)
-  drow()
+
+  return points;
 }
-// function drow() {
-//   const div = document.createElement('div')
-//   console.log(weekly)
-//   weekly - 1 === 0
-//     ? (div.className = `flex flex-col border absolute top-[0]`)
-//     : (div.className = `flex flex-col border absolute top-[${
-//         (weekly - 1) * 100
-//       }px]`)
-//   let divInner = ``
-//   for (let i = 0; i < divCount.length; i++) {
-//     if (i === 0) {
-//       console.log(`divcount ${divCount[i]}`)
-//       // console.log(preCount * 100)
-//       divInner += `
-//       <div class="flex justify-end w-[700px]">
-//         <div class="w-[${divCount[i] * 100}px] h-[100px] border  bg-purple-100">
-//         </div>
-//       </div>
 
-//       `
-//     } else {
-//       divInner += `
-//       <div class="w-[${divCount[i] * 100}px] h-[100px] border   bg-purple-100">
-//       </div>
-//       `
-//     }
-//   }
-//   console.log(div)
-//   div.innerHTML = divInner
-//   container.appendChild(div)
-// }
+const pt = svg.node.createSVGPoint();
+function transformCoords(evt) {
+  pt.x = evt.clientX;
+  pt.y = evt.clientY;
+
+  return pt.matrixTransform(svg.node.getScreenCTM().inverse());
+}
+
+const range = 40;
+
+(function animate() {
+  if (window.mouseHasMoved) {
+    points.forEach((p, index) => {
+      const point = new Vector2D(pointOrigins[index].x, pointOrigins[index].y);
+      const d = Vector2D.sub(point, mousePos);
+      const l = Math.hypot(d.x, d.y);
+      let y;
+
+      if (l < range && !p.reset) {
+        point.sub(new Vector2D(d.x, -(d.y * 0.675)));
+        y = point.y;
+      } else {
+        y = pointOrigins[index].y;
+      }
+
+      p.y += (y - p.y) * 0.1;
+
+      circles[index].attr("cy", p.y);
+    });
+
+    bgPoints.forEach((p, index) => {
+      const point = new Vector2D(pointOrigins[index].x, pointOrigins[index].y);
+      const d = Vector2D.sub(point, mousePos);
+      const l = Math.hypot(d.x, d.y);
+      let y;
+
+      if (l < range && !p.reset) {
+        point.sub(new Vector2D(d.x, -(d.y * 0.675)));
+        y = point.y;
+      } else {
+        y = pointOrigins[index].y;
+      }
+
+      p.y += (y - p.y) * 0.05;
+    });
+  }
+
+  bgPathData = spline(bgPoints, 1, true);
+  bgPath.attr("d", bgPathData);
+
+  btnPathData = spline(points, 1, true);
+  btnPath.attr("d", btnPathData);
+
+  requestAnimationFrame(animate);
+})();
+
+window.addEventListener("mousemove", (e) => {
+  const { x, y } = transformCoords(e);
+
+  mousePos.x = x;
+  mousePos.y = y;
+
+  mouseHasMoved = true;
+
+  points.forEach((p, index) => {
+    const point = new Vector2D(pointOrigins[index].x, pointOrigins[index].y);
+    const d = Vector2D.sub(point, mousePos);
+    const l = Math.hypot(d.x, d.y);
+
+    if (l < range) {
+      p.reset = false;
+      debounce(() => {
+        p.reset = true;
+      }, 200)();
+    } else {
+      p.reset = false;
+    }
+  });
+
+  bgPoints.forEach((p, index) => {
+    const point = new Vector2D(pointOrigins[index].x, pointOrigins[index].y);
+    const d = Vector2D.sub(point, mousePos);
+    const l = Math.hypot(d.x, d.y);
+
+    if (l < range) {
+      p.reset = false;
+      debounce(() => {
+        p.reset = true;
+      }, 200)();
+    } else {
+      p.reset = false;
+    }
+  });
+});
+
+document.querySelector(".debug-text").addEventListener("click", (e) => {
+  if (document.body.classList.contains("debug")) {
+    document.body.classList.remove("debug");
+  } else {
+    document.body.classList += "debug";
+  }
+});
